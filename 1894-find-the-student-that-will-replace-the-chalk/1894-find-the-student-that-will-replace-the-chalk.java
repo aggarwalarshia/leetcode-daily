a49cbe29e6e4483069d3edeb1f1d6e8a1b8dd603
aggarwalarshia <160538181+aggarwalarshia@users.noreply.github.com>
2024-09-02 09:43:04 +0530

    Time: 2 ms (72.59%), Space: 56.6 MB (98.90%) - LeetHub

--- 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.java
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.java
@@ -1,19 +1,40 @@
 class Solution {
-    public int chalkReplacer(int[] chalk, int k) {
-        long totalChalk=0;
-        for(int i:chalk)
+    int n;
+    public int ceil(long prefixSum[], int remainingChalks)
+    {
+        int start=0;
+        int end=n-1;
+        int ans=0;
+        while(start<=end)
         {
-            totalChalk+=i;
-        }
-        k=(int)(k%totalChalk);
-        for(int i=0;i<chalk.length;i++)
-        {
-            if(k<chalk[i])
+            int mid=start+(end-start)/2;
+            if(prefixSum[mid]==remainingChalks)
             {
-                return i;
+                return mid+1;
             }
-            k-=chalk[i];
+            else if(prefixSum[mid]<remainingChalks)
+            {
+                start=mid+1;
+            }
+            else
+            {
+                ans=mid;
+                end=mid-1;
+            }
         }
-        return -1;
+        return ans;
+    }
+    public int chalkReplacer(int[] chalk, int k) {
+        n=chalk.length;
+        long prefixSum[]=new long[n];
+        prefixSum[0]=chalk[0];
+        for(int i=1;i<n;i++)
+        {
+            prefixSum[i]=prefixSum[i-1]+chalk[i];
+        }
+        long sum=prefixSum[n-1];
+        int remainingChalks=(int)(k%sum);
+
+        return ceil(prefixSum,remainingChalks);
     }
 }
